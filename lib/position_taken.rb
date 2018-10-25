82c278c78f2e3f7cd6bb45e113f365d19aa6964a
S.O <shakirat.odunsi@gmail.com>
2018-10-25 15:30:28 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,13 @@
 def position_taken?(board,index)
   if
-    board[index] == " " || "" ||  nil
+    board[index] == " " 
       false
     elsif
+      board[index] == "" 
+      false
+    elsif board[index] == ||  nil
+      false
+    elsif 
       board[index] == "X" || "O"
       true
   end
